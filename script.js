63116d621cf622c8adb5221284b93bb34a97df97
MirantosConcursos <ronaldoaugusto.facilitador@gmail.com>
2024-10-05 12:05:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,100 @@
 const flashcards = [
     {
-        question: "Com base na Lei 10.826/2003 - Dispõe sobre registro, posse e comercialização de armas de fogo e munição, sobre o Sistema Nacional de Armas – SINARM,  O SINARM é responsável por:",
+        question: "O SINARM é responsável por:",
         answer: "Identificar as características e a propriedade de armas de fogo, mediante cadastro."
     },
     {
-        question: "Com base na Lei 10.826/2003 - Dispõe sobre registro, posse e comercialização de armas de fogo e munição, sobre o Sistema Nacional de Armas – SINARM,  È competência do SINARM",
+        question: "È competência do SINARM",
         answer: "cadastrar as transferências de propriedade, extravio, furto, roubo e outras ocorrências suscetíveis de alterar os dados cadastrais."
     },
     {
 
-        question: "Com base na Lei 10.826/2003 - Dispõe sobre registro, posse e comercialização de armas de fogo e munição, sobre o Sistema Nacional de Armas – SINARM. Quem é responsável por realizar os testes de marcação e raiamento do cano da arma para cadastro no SINARM:",
+        question: "Quem é responsável por realizar os testes de marcação e raiamento do cano da arma para cadastro no SINARM:",
         answer: " O fabricante da arma"
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "As disposições do SINARM, NÃO se aplicam a:",
+        answer: "Armas de fogo das Forças Armadas."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "O SINARM deve cadastrar quais das seguintes entidades?",
+        answer: " Produtores, atacadistas, varejistas, exportadores e importadores autorizados de armas de fogo, acessórios e munições"
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "Sobre o Sistema Nacional de Armas – SINARM. De acordo com o Art. 3º, qual é a obrigação relacionada ao registro de armas de fogo?",
+        answer: "O registro é obrigatório no órgão competente."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "Sobre o Sistema Nacional de Armas – SINARM.   Onde devem ser registradas as armas de fogo de uso restrito:",
+        answer: "No Comando do Exército, conforme o regulamento da Lei."
     },
     {
 
-    question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+    question: "Sobre o Sistema Nacional de Armas – SINARM.  Qual é a forma de registro para armas de fogo de uso restrito?",
+        answer: "Deve ser feito no Comando do Exército de acordo com regulamento específico"
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "Sobre o Sistema Nacional de Armas – SINARM. o registro de armas de fogo é obrigatório para:",
+        answer: "Todas as armas de fogo, sem exceção."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "MTP 01 - Qual é um dos objetivos do Código de Conduta para os Encarregados pela Aplicação da Lei (CCEAL)?",
+        answer: "Estabelecer diretrizes para a conduta policial."
     },
     {
 
-     question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+     question: "MTP 01 - Qual documento mencionado visa combater a tortura e outros tratamentos cruéis?",
+        answer: "Convenção Contra a Tortura e outros Tratamentos ou Penas Cruéis, Desumanas ou Degradantes."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "Qual é o principal desafio mencionado em relação à atividade policial-militar?",
+        answer: "A complexidade da atividade policial-militar."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "Sobre o Sistema Nacional de Armas – SINARM. O que é exigido para comprovar a ocupação e residência do interessado na aquisição de uma arma de fogo de uso permitido:",
+        answer: "Documento comprovando a ocupação lícita e residência certa."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "Sobre o Sistema Nacional de Armas – SINARM. As certidões negativas de antecedentes criminais podem ser fornecidas por:",
+        answer: "Meios eletrônicos, conforme permitido."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "MTP 01 - O que é considerado imprescindível na atuação da Polícia Militar, segundo o texto?",
+        answer: "Respeitar princípios legais e éticos."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "MTP 01 - O que torna cada ocorrência policial única, segundo o texto?",
+        answer: "O conjunto de variáveis envolvidas."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "MTP 01 - O que está diretamente relacionado à segurança do policial militar?",
+        answer: "O preparo mental."
     },
     {
 
-        question: "Qual é a capital do Brasil?",
-        answer: "Brasília"
+        question: "MTP 01 - Qual a importância do preparo mental no cotidiano operacional do policial militar?",
+        answer: "É fundamental para a eficácia e segurança nas intervenções."
     },
     {
 
-        question: "Qual é o maior planeta do sistema solar?",
-        answer: "Júpiter"
+        question: "Sobre o Sistema Nacional de Armas – SINARM. Quem expede a autorização de compra de arma de fogo?",
+        answer: " O Sinarm."
     },
     {
-        question: "Quem escreveu 'Dom Casmurro'?",
-        answer: "Machado de Assis"
+        question: "Sobre o Sistema Nacional de Armas – SINARM. A autorização de compra de arma de fogo É: ",
+        answer: "Em nome do requerente e para a arma indicada, sendo intransferível."
     }
 ];
 
